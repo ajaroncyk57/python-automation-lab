0a5b1234df7fd95e1c57ff1c2e762cce207dd909
Andrew Jaroncyk <80014081+ajaroncyk57@users.noreply.github.com>
2026-04-27 20:00:45 -0400

    Add PasswordVerification.py

---
+++ archive/intro-python-exercises/PasswordVerification.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 00:04:32 2019
+
+@author: Andrew Jaroncyk
+"""
+
+user_password = input("Enter a password: ")
+user_password_2 = input("Re-enter your password: ")
+
+if user_password == user_password_2:
+    print(":)")
+else: 
+    print(">:(")
